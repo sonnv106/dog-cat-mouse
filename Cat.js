f40ed7f0e5146cca692171e88449658a9b385c50
sonnguyen <sonnguyen106mh@gmail.com>
2020-03-08 10:28:40 +0700

    add stomach property for cat

--- Cat.js
+++ Cat.js
@@ -1,4 +1,12 @@
 function Cat(){
-
+	this.stomach=[]
 }
 module.exports=Cat;
+
+//git init
+//git status xem trang thai hien tai
+//git add them file vao
+//git commit : push file len git
+//git log : xem lich su push
+//git show 
+//git diff xem lai su thay doi trong code
